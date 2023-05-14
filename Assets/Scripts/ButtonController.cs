90bacb15bf860193fbea62086cb6f1029d49021f
makkenzo <nekgo2009@gmail.com>
2023-05-14 19:08:19 +0600

    button color switch

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftButtonController : MonoBehaviour
+{
+    public Renderer buttonRenderer;
+    public GameObject door;
+
+    private bool doorIsOpen;
+
+    private void Start()
+    {
+        doorIsOpen = door.GetComponent<Animator>().GetBool("isOpened");
+    }
+
+    private void Update()
+    {
+        doorIsOpen = door.GetComponent<Animator>().GetBool("isOpened");
+
+        if (doorIsOpen)
+        {
+            buttonRenderer.material.color = new Color(0f, 1f, 0.02951145f);
+        }
+        else
+        {
+            buttonRenderer.material.color = new Color(1f, 0f, 0.1419296f);
+        }
+    }
+}

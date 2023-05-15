b00c7182c66784cee612b94c96e441d3d17e35c0
Vlad <79857355+vladgerman27@users.noreply.github.com>
2023-05-15 10:03:17 +0600

    Add files via upload

--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class TimeController : MonoBehaviour
@@ -8,6 +9,7 @@ public class TimeController : MonoBehaviour
     public Text TimeText;
     public int number = 0;
     private float timer = 0f;
+    private int count = 0;
 
     void Update()
     {
@@ -16,7 +18,16 @@ public class TimeController : MonoBehaviour
         {
             number++;
             timer = 0f;
+            count++;
             TimeText.text = number.ToString() + " AM";
+            if (count == 6)
+            {
+                SceneManager.LoadScene("Victory");
+                if (timer == 10f)
+                {
+                    SceneManager.LoadScene("Menu");
+                }
+            }
         }
     }
 }
